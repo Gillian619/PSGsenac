a1fde6a24765dd035373b55398ba4de918074b8d
IsisTheOne <alenacard619@gmail.com>
2023-05-25 17:22:49 -0300

    25/05/2023

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function calcularIdade() {
     const hoje = new Date();
     const nasc = new Date(document.getElementById("BirthDate").value);
-    const ano = hoje.getFullYear() - nasc.getFullYear();
+    let ano = hoje.getFullYear() - nasc.getFullYear();
     const mes = hoje.getMonth() - nasc.getMonth();
     if (mes < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) {
         age--;
