82dae3f6fee8e4b8a49756630300792515bd86c9
IsisTheOne <alenacard619@gmail.com>
2023-05-30 17:30:47 -0300

    30/05/2023

--- js/script.js
+++ js/script.js
@@ -20,37 +20,39 @@ function matricula() {
     }
 }
 function notamedia() {
-    let n1 = parseFloat(document.getElementById("n1").value)
-    let n2 = parseFloat(document.getElementById("n2").value)
-    let n3 = parseFloat(document.getElementById("n3").value)
+    let n1 = parseFloat(document.getElementById("notaa").value)
+    let n2 = parseFloat(document.getElementById("notab").value)
+    let n3 = parseFloat(document.getElementById("notac").value)
     let nm = ((n1 + n2 + n3) / 3).toFixed(2);
+    return nm
 }
 function presenca() {
-    let pr1 = parseFloat(document.querySelector('input[name="flexRadiopresd1"]:checked').value);
-    let pr2 = parseFloat(document.querySelector('input[name="flexRadiopresd2"]:checked').value);
-    let pr3 = parseFloat(document.querySelector('input[name="flexRadiopresd3"]:checked').value);
-    let pr4 = parseFloat(document.querySelector('input[name="flexRadiopresd4"]:checked').value);
-    let pr5 = parseFloat(document.querySelector('input[name="flexRadiopresd5"]:checked').value);
-    let pr6 = parseFloat(document.querySelector('input[name="flexRadiopresd6"]:checked').value);
-    let pr7 = parseFloat(document.querySelector('input[name="flexRadiopresd7"]:checked').value);
-    let pr8 = parseFloat(document.querySelector('input[name="flexRadiopresd8"]:checked').value);
-    let pr9 = parseFloat(document.querySelector('input[name="flexRadiopresd9"]:checked').value);
-    let pr10 = parseFloat(document.querySelector('input[name="flexRadiopresd10"]:checked').value);
-    let pr11 = parseFloat(document.querySelector('input[name="flexRadiopresd11"]:checked').value);
-    let pr12 = parseFloat(document.querySelector('input[name="flexRadiopresd12"]:checked').value);
+    let pr1 = parseInt(document.querySelector('input[name="flexRadio1"]:checked').value)
+    let pr2 = parseFloat(document.querySelector('input[name="flexRadio2"]:checked').value);
+    let pr3 = parseFloat(document.querySelector('input[name="flexRadio3"]:checked').value);
+    let pr4 = parseFloat(document.querySelector('input[name="flexRadio4"]:checked').value);
+    let pr5 = parseFloat(document.querySelector('input[name="flexRadio5"]:checked').value);
+    let pr6 = parseFloat(document.querySelector('input[name="flexRadio6"]:checked').value);
+    let pr7 = parseFloat(document.querySelector('input[name="flexRadio7"]:checked').value);
+    let pr8 = parseFloat(document.querySelector('input[name="flexRadio8"]:checked').value);
+    let pr9 = parseFloat(document.querySelector('input[name="flexRadio9"]:checked').value);
+    let pr10 = parseFloat(document.querySelector('input[name="flexRadio10"]:checked').value);
+    let pr11 = parseFloat(document.querySelector('input[name="flexRadio11"]:checked').value);
+    let pr12 = parseFloat(document.querySelector('input[name="flexRadio12"]:checked').value);
     let pre = (pr1 + pr2 + pr3 + pr4 + pr5 + pr6 + pr7 + pr8 + pr9 + pr10 + pr11 + pr12)
     let prm = ((pre / 12) * 100)
+    return prm
 }
 function aproved() {
-    let note = (notamedia)
-    let pres = (presenca)
-    if (note >= 6 && pres >= 75) {
-        document.getElementById("results").innerHTML = "Aprovado"
+    let note = notamedia()
+    let pres = presenca()
+    if (note >= 6.0 && pres >= 75.0) {
+        document.getElementById("results").innerHTML = ("Aprovado")
     }
-    else if (note > 5 < 6 && pres > 8) {
-        document.getElementById("results").innerHTML = "Recuperação"
+    else if (note > 5.0 && note < 6.0 && pres >= 75.0) {
+        document.getElementById("results").innerHTML = ("Recuperação")
     }
     else {
-        document.getElementById("results").innerHTML = "reprovado"
+        document.getElementById("results").innerHTML = ("reprovado")
     }
 }
